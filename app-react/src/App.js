f21ff7b2303a7dfa096eb6f53ee8e4971f9843af
CatalinaMarcos <cmarcosaslanian@udesa.edu.ar>
2025-09-01 12:47:18 -0300

    arrancamos header y footer

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import {Route, Switch} from "react-router-dom"
+
 function App() {
   return (
-  <p>React</p>
+  <React.Fragment>
+    <Header />
+  
+    <Footer />
+  </React.Fragment>
 )
 }
 
---
+++ app-react/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./styles.css"
+
+ function Footer() {
+    return(
+        <footer>
+        <h3>Catalina Marcos</h3>
+        <h3>Lucas Borestein</h3>
+        <h3>Margarita Souviron</h3>
+        </footer>
+    );
+ }
+
+ export default Footer;
