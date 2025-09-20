54cdebb557bb4323042b9d0497821150550354f6
CatalinaMarcos <cmarcosaslanian@udesa.edu.ar>
2025-09-20 17:08:14 -0300

    pagina notfound terminada

--- app-react/src/screens/NotFound/NotFound.js
+++ app-react/src/screens/NotFound/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react';
-function NotFound(){
-    return(
-    <p>NotFound</p>
-);
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+function NotFound() {
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-message">¡Ups! La página que buscas no existe.</p>
+      <Link to="/" className="notfound-link">Volver al inicio</Link>
+    </div>
+  );
 }
+
 export default NotFound;
